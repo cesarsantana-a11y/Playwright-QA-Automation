742aa322148c96922fe5451a433bd0762766b121
cesarsantana-a11y <your@email.com>
2026-06-23 22:53:01 -0400

    GetbyRole

---
+++ tests/Locators/GetbyRole.spec.ts
@@ -0,0 +1,22 @@
+import {test, expect} from '@playwright/test';
+
+test('Get by role', async ({ page }) => { 
+  await page.goto('https://www.rockstargames.com/');
+  const rejectbutton = await page.getByRole('button', { name: 'REJECT ALL' });  
+    await expect(rejectbutton).toBeVisible();
+    await rejectbutton.click();
+})
+  
+test('Get by role - accept', async ({ page }) => {
+  await page.goto('https://www.rockstargames.com/');
+  const acceptbutton = await page.getByRole('button', { name: 'ACCEPT ALL' });
+  await expect(acceptbutton).toBeVisible();
+  await acceptbutton.click();
+})
+
+// test('Get by role - link', async ({ page }) => {
+//     await page.goto('https://www.rockstargames.com/');
+//     const link = page.getByRole('link', { name: 'Watch Trailer', exact: true });
+//     await expect(link).toBeVisible();
+//     await link.click();
+// })
